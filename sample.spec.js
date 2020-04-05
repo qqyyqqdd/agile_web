e61bf8b29419e4c8cd1143112d6d8eb9c04f517d
qqyyqqdd <957292985@qq.com>
2020-04-05 21:24:26 +0800

    homework6

---
+++ sample.spec.js
@@ -0,0 +1,39 @@
+describe('add todo', function () {
+    let page;
+
+    before (async function () {
+      page = await browser.newPage();
+      await page.goto('http://127.0.0.1:7001/');
+    });
+  
+    after (async function () {
+      await page.close();
+    });
+
+    it('should have correct title', async function() {
+        expect(await page.title()).to.eql('Koa • Todo');
+    })
+    it('should new todo correct', async function() {
+      await page.click('#new-todo', {delay: 500});
+      await page.type('#new-todo', 'new todo item', {delay: 50});
+      await page.keyboard.press("Enter");
+      let todoList = await page.waitFor('#todo-list');
+      const expectInputContent = await page.evaluate(todoList => todoList.lastChild.querySelector('label').textContent, todoList);
+      expect(expectInputContent).to.eql('new todo item');
+    }) 
+    it('should items amount correct',async function() {
+    	let todoList = await page.waitFor('#todo-list');
+    	let expectItemAmount = await page.evaluate(()=>{
+    		var list = document.getElementById('todo-list');
+    		return list.getElementsByTagName('li').length;
+    	})
+    	
+    	expect(expectItemAmount).to.eql(4);
+    })
+    it('should finish item correct',async function(){
+    	let todoList = await page.waitFor('#todo-list');
+    	await page.click('.toggle', {delay: 500});
+    	let finishItem = await page.evaluate(todoList => todoList.firstChild.querySelector('input').checked, todoList);    	
+    	expect(finishItem).to.eql(false);
+    })
+  });
